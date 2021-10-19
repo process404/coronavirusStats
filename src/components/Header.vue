818ce7099a6262b9f59a25344c55f853450b8600
88147999+exeProgram@users.noreply.github.com <88147999+exeProgram@users.noreply.github.com>
2021-10-20 00:09:39 +0100

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header>
     <div class="flex justify-center items-center h-auto backdrop: bg-green-600 mb-3 flex-col gap-2 pb-5 pt-5">
       <h1 class="font-bold text-white w-full text-left pl-5 text-2xl md:text-3xl">Covid 19 Tracker</h1>
-      <p class="font-normal text-white w-full text-left pl-5">API By <a class="text-black hover:text-white " href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+      <p class="font-normal text-white w-full text-left pl-5">Using an API by <a class="text-black hover:text-white " href="https://covid19api.com" target="_blank">covid19api.com</a></p>
     </div>
   </header>
 </template>
