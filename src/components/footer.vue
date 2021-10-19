29cd0268c2534e1be64e8f47db8b397bb9b6e852
88147999+exeProgram@users.noreply.github.com <88147999+exeProgram@users.noreply.github.com>
2021-10-20 00:41:20 +0100

    improvement 2

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,13 +4,15 @@
         <h2 class="text-white">Built using <a class="text-vue-color hover:text-vue-color-hglt cursor-pointer" href="https://vuejs.org/">Vue</a> 
         and 
         <a href="https://tailwindcss.com/" class="text-tailwind-col cursor-pointer hover:text-tailwind-col-hglt">Tailwind</a>
-        . Using a tutorial from 
+         using a tutorial from 
         <a href="https://www.youtube.com/watch?v=m-MAIpnH9ag" class="text-red-800 hover:text-red-400">Youtube</a>
         
         </h2>
       </div>
-      <div class="sm:w-auto md:w-1/3 flex items-center justify-end pr-5">
-        <h2 class="text-white md:text-right sm:text-left md:pl-0 md:pt-0 sm:pl-5 sm:pt-5 font-light"><span class="font-semibold">Problems?   </span>The API may have reached the rate limit</h2>
+      <div class="sm:w-auto md:w-1/3 flex pr-5 flex-col">
+        <h2 class="text-white md:text-right sm:text-left md:pl-0 md:pt-0 sm:pl-5 sm:pt-5 font-light">This site may stop working if the rate limit of the API is reached.</h2>
+        <br>
+        <h2 class="text-white md:text-right sm:text-left md:pl-0 md:pt-0 sm:pl-5 sm:pt-5 font-light">Accuracy of data is dependant on the sources that the API uses</h2>
       </div>
   </div>
 </template>
